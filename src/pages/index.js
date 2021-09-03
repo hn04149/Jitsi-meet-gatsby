b0dd49c8f3981ebbb92fca2611d986e8941c0cc2
Lee Sangjin <hn04147@fgb.ai>
2021-09-03 17:09:42 +0900

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,12 @@ import Jitsi from "./jitsi-gatsby"
 
 const IndexPage = () => {
   React.useEffect(() => {
-    const script1 = document.createElement("script")
-    script1.src = "https://meet.jit.si/external_api.js"
-    script1.async = true
-
-    const script2 = document.createElement("script")
-    script2.src = "https://code.jquery.com/jquery-3.5.1.min.js"
-    script2.async = true
-
-    const script3 = document.createElement("script")
-    script3.src = "https://meet.jit.si/libs/lib-jitsi-meet.min.js"
-    script3.async = true
-
-    document.body.appendChild(script1)
-    document.body.appendChild(script2)
-    document.body.appendChild(script3)
-
+    const script = document.createElement("script")
+    script.src = "https://meet.jit.si/external_api.js"
+    script.async = true
+    document.body.appendChild(script)
     return () => {
-      document.body.removeChild(script1)
-      document.body.removeChild(script2)
-      document.body.removeChild(script3)
+      document.body.removeChild(script)
     }
   }, [])
 
@@ -62,7 +48,7 @@ const Popup = ({ onClose, roomName, yourName, password }) => {
     <div>
       <Main>
         <CloseBtn onClick={onClose}>Close</CloseBtn>
-        <div style={{ position: "absolute", top: "60px" }}>
+        <div style={{ position: "absolute", top: "100px" }}>
           <Jitsi roomName={roomName} yourName={yourName} password={password} />
         </div>
       </Main>
--- src/pages/jitsi-gatsby.jsx
+++ src/pages/jitsi-gatsby.jsx
@@ -1,29 +1,61 @@
 import * as React from "react"
-import styled from "styled-components"
 import ProgressComponent from "@material-ui/core/CircularProgress"
+import { MdVideocamOff, MdVideocam, MdDashboard, MdScreenShare } from "react-icons/md"
+import { BiRectangle } from "react-icons/bi"
+import { FaMicrophone, FaMicrophoneSlash, FaRegHandPaper } from "react-icons/fa"
+import { RiChat4Fill, RiChatOffFill } from "react-icons/ri"
 
-const Jitsi = ({ roomName, yourName, password }) => {
+const JitsiScreen = ({ roomName, yourName, onClose }) => {
+  const [api, setApi] = React.useState()
   const [loading, setLoading] = React.useState(true)
+  const [mouseOn, setMouseOn] = React.useState("undefined")
+  const [setting, setSetting] = React.useState({
+    videomuted: false,
+    audiomuted: false,
+    chat: false,
+    isTileView: false,
+    raisehand: false,
+  })
+  const [privateChatId, setPrivateChatId] = React.useState("")
+
   const jitsiContainerStyle = {
     display: loading ? "none" : "block",
-    width: "100%",
-    height: "100%",
+    width: "1000px",
+    height: "700px",
+    paddingTop: "50px",
   }
 
-  async function startConference () {
+  async function startConference() {
     try {
       const domain = "meet.jit.si"
       const options = {
         roomName: roomName,
-        width: 1400,
-        height: 700,
+        width: "100%",
+        height: "100%",
         configOverwrite: {
-          prejoinPageEnabled: false,
+          doNotStoreRoom: true,
+          startVideoMuted: 0,
+          startWithVideoMuted: true,
           startWithAudioMuted: true,
+          enableWelcomePage: false,
+          prejoinPageEnabled: false,
+          disableRemoteMute: true,
+          remoteVideoMenu: {
+            disableKick: true,
+          },
         },
         interfaceConfigOverwrite: {
-          SHOW_JITSI_WATERMARK: false,
           filmStripOnly: false,
+          SHOW_JITSI_WATERMARK: false,
+          SHOW_WATERMARK_FOR_GUESTS: false,
+          HIDE_DEEP_LINKING_LOGO: true,
+          SHOW_BRAND_WATERMARK: false,
+          SHOW_POWERED_BY: false,
+          DEFAULT_BACKGROUND: "black",
+          INITIAL_TOOLBAR_TIMEOUT: 2000,
+          TOOLBAR_TIMEOUT: 2000,
+          TOOLBAR_ALWAYS_VISIBLE: false,
+          //TOOLBAR_BUTTONS: ["fullscreen", "settings"],
         },
         parentNode: document.querySelector("#jitsi-iframe"),
         userInfo: {
@@ -31,22 +63,73 @@ const Jitsi = ({ roomName, yourName, password }) => {
         },
       }
 
-      const api = new window.JitsiMeetExternalAPI(domain, options)
+      const _api = new window.JitsiMeetExternalAPI(domain, options)
+      setApi(_api)
 
-      //get available device List
-      const availableDeviceList = await api.getAvailableDevices()
-
-      //current URL
-      const liveStreamUrl = await api.getLivestreamUrl()
-
-      api.addEventListener("videoConferenceJoined", () => {
-        console.log("Local User Joined")
-        console.log(availableDeviceList)
-        console.log(liveStreamUrl)
+      _api.addEventListener("videoConferenceJoined", async ({ roonName, id, displayName, avatarURL }) => {
+        console.log("Local User Joined", roomName, id, displayName, avatarURL)
         setLoading(false)
+
+        const audio = await _api.isAudioMuted()
+        const video = await _api.isVideoMuted()
+        setSetting({
+          ...setting,
+          videomuted: video,
+          audiomuted: audio,
+        })
+      })
+
+      _api.addEventListener("mouseEnter", async event => {
+        console.log(event)
       })
     } catch (e) {
       console.error("Failed to load Jitsi API", e)
+    }
+  }
+
+  
+
+  async function Execute(command, options) {
+    if (command === "toggleAudio") {
+      await api.executeCommand("toggleAudio")
+      const result = await api.isAudioMuted()
+      if (result !== setting.audiomuted) {
+        setSetting({
+          ...setting,
+          audiomuted: result,
+        })
+      }
+    } else if (command === "toggleVideo") {
+      await api.executeCommand("toggleVideo")
+      const result = await api.isVideoMuted()
+      if (result !== setting.videomuted) {
+        setSetting({
+          ...setting,
+          videomuted: result,
+        })
+      }
+    } else if (command === "toggleTileView") {
+      await api.executeCommand("toggleTileView")
+      setSetting({
+        ...setting,
+        isTileView: !setting.isTileView,
+      })
+    } else if (command === "toggleChat") {
+      await api.executeCommand("toggleChat")
+      setSetting({
+        ...setting,
+        chat: !setting.chat,
+      })
+    } else if (command === "toggleShareScreen") {
+      await api.executeCommand("toggleShareScreen")
+    } else if (command === "toggleRaiseHand") {
+      await api.executeCommand("toggleRaiseHand")
+      setSetting({
+        ...setting,
+        raisehand: !setting.raisehand,
+      })
+    } else if (command === "initiatePrivateChat") {
+      api.executeCommand("initiatePrivateChat", options.participantID)
     }
   }
 
@@ -55,24 +138,207 @@ const Jitsi = ({ roomName, yourName, password }) => {
     else alert("API script not loaded")
   }, [])
 
+  async function getParticipantsInfo() {
+    const result = await api.getParticipantsInfo()
+    console.log(result)
+  }
+
   return (
-    <div style={{ width: "100%" }}>
-      {loading && (
+    <>
+      <div style={{ width: "100%" }}>
+        {loading && (
+          <div
+            style={{
+              height: "100%",
+              width: "100%",
+              lineHeight: "700px",
+              textAlign: "center",
+              display: "inline-block",
+            }}
+          >
+            <ProgressComponent />
+          </div>
+        )}
+        <div id="jitsi-iframe" style={jitsiContainerStyle}></div>
         <div
           style={{
-            height: "700px",
-            width: "1400px",
-            lineHeight: "700px",
-            textAlign: "center",
-            display: "inline-block",
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
           }}
         >
-          <ProgressComponent />
+          {!loading && (
+            <div style={{ position: "relative", marginTop: "20px" }}>
+              {setting.audiomuted ? (
+                <FaMicrophoneSlash
+                  className="icons"
+                  onClick={() => Execute("toggleAudio")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Microphone" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Microphone")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              ) : (
+                <FaMicrophone
+                  className="icons"
+                  onClick={() => Execute("toggleAudio")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Microphone" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Microphone")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              )}
+              {setting.videomuted ? (
+                <MdVideocamOff
+                  className="icons"
+                  onClick={() => Execute("toggleVideo")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Video" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Video")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              ) : (
+                <MdVideocam
+                  className="icons"
+                  onClick={() => Execute("toggleVideo")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Video" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Video")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              )}
+              {setting.chat ? (
+                <RiChat4Fill
+                  className="icons"
+                  onClick={() => Execute("toggleChat")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Chat" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Chat")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              ) : (
+                <RiChatOffFill
+                  className="icons"
+                  onClick={() => Execute("toggleChat")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Chat" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Chat")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              )}
+              {setting.isTileView ? (
+                <MdDashboard
+                  className="icons"
+                  onClick={() => Execute("toggleTileView")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Dashboard" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Dashboard")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              ) : (
+                <BiRectangle
+                  className="icons"
+                  onClick={() => Execute("toggleTileView")}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "30px",
+                    cursor: "pointer",
+                    background: mouseOn === "Dashboard" ? "#adadad" : null,
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={() => setMouseOn("Dashboard")}
+                  onMouseLeave={() => setMouseOn("undefined")}
+                />
+              )}
+              <MdScreenShare
+                className="icons"
+                onClick={() => Execute("toggleShareScreen")}
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  marginRight: "30px",
+                  cursor: "pointer",
+                  background: mouseOn === "ScreenShare" ? "#adadad" : null,
+                  borderRadius: "5px",
+                }}
+                onMouseEnter={() => setMouseOn("ScreenShare")}
+                onMouseLeave={() => setMouseOn("undefined")}
+              />
+              <FaRegHandPaper
+                className="icons"
+                onClick={() => Execute("toggleRaiseHand")}
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  marginRight: "30px",
+                  padding: "2px",
+                  background:
+                    mouseOn === "RaiseHand"
+                      ? "#adadad"
+                      : setting.raisehand
+                      ? "#c9c9c9"
+                      : "#ebebeb",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+                onMouseEnter={() => setMouseOn("RaiseHand")}
+                onMouseLeave={() => setMouseOn("undefined")}
+              />
+            </div>
+          )}
+
+          <div>
+            <button onClick={getParticipantsInfo}>Get Participants Info</button>
+            <input value={privateChatId} onChange={(e) => setPrivateChatId(e.target.value)} />
+            <button onClick={() => Execute("initiatePrivateChat", {participantID: privateChatId})}>Initiate Private Chat</button>
+          </div>
         </div>
-      )}
-      <div id="jitsi-iframe" style={jitsiContainerStyle}></div>
-    </div>
+      </div>
+    </>
   )
 }
 
-export default Jitsi
+export default JitsiScreen
